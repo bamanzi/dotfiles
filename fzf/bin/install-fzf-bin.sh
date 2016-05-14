d15e46b17d139e9d0be832a36070716f7e863ce5
Ba Manzi <bamanzi@gmail.com>
2016-05-14 16:14:40 +0800

    install-fzf-bin.sh: workaround for ruby version

--- fzf/bin/install-fzf-bin.sh
+++ fzf/bin/install-fzf-bin.sh
@@ -6,11 +6,16 @@ case $arch in
 #  armv7*) bin_arch=android_arm7;;
   x86_64) bin_arch=linux_amd64;;
   i386|i586|i686)  bin_arch=linux_386;;
-  *) print "no binary for arch '$arch'."; exit -1;;
+  *)
+    print "no binary for arch '$arch'. fallback to ruby version"
+    ln -s fzf.rb fzf
+    exit 2
+    ;;
 esac
 
 [ -d ~/temp ] || mkdir ~/temp
 (cd ~/temp && wget -c "https://github.com/junegunn/fzf-bin/releases/download/${VER}/fzf-${VER}-${bin_arch}.tgz" && tar zxvf fzf-${VER}-${bin_arch}.tgz )
 
 cp -f ~/temp/fzf-${VER}-${bin_arch} fzf-${bin_arch}
+[ -f fzf ] && mv fzf fzf.bak
 ln -s fzf-${bin_arch} fzf
